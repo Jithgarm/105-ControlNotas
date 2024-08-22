17fc179883645c8f0121148b3147f7a5120f6c63
Jithgarm <jonathansmg919@gmail.com>
2024-08-22 07:59:00 -0500

    actualización

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require("express")//Traer express
-
+const mongoose = require("mongoose")
+require("dotenv").config()
 
 const app = express()//Asignando constante a express
 const port = 3005//Determinar puerto
 
-app.set("port",port)//Set=configuración, dentro de configuración le digo que sea el puerto 3005 (el port)
+//Set=configuración, dentro de configuración le digo que sea el puerto 3005 (el port)
+app.set("port",port)
 
-app.get("/",(req,res)=>{//endpoint, ruta que voy a consultar, req = lo que envían, res = me devuelven la información con get
+//Conectar usando string utilizado
+mongoose.connect(process.env.MONGO_DB_URI)
+.then(()=>console.log("Conectado a la BD"))
+.catch((err)=>console.error(err.message))
+
+//endpoint, ruta que voy a consultar, req = lo que envían, res = me devuelven la información con get
+app.get("/",(req,res)=>{
     res.send("Probando el servidor")
 })
+
+
 
 app.listen(port,()=>{
     console.log(`Escuchando el ${port}`)
